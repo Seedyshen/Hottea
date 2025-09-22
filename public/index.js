70e4c387502f907ca68e8ee2cc560d9575cce71a
seedy <seedyshen@gmail.com>
2025-09-22 13:52:32 +0800

    feat: add What happens after you report page with dual route support (hyphen and underscore)

--- public/index.js
+++ public/index.js
@@ -86,6 +86,27 @@ app.get('/taking_action', (req, res) => {
   res.sendFile(path.join(__dirname, 'taking-action.html'));
 });
 
+// 处理what-happens-after-you-report路由（支持连字符和下划线两种格式）
+app.get('/what-happens-after-you-report', (req, res) => {
+  // 设置缓存控制头，强制刷新缓存
+  res.set({
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0'
+  });
+  res.sendFile(path.join(__dirname, 'what-happens-after-you-report.html'));
+});
+
+app.get('/what_happens_after_you_report', (req, res) => {
+  // 设置缓存控制头，强制刷新缓存
+  res.set({
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0'
+  });
+  res.sendFile(path.join(__dirname, 'what-happens-after-you-report.html'));
+});
+
 // 处理根路径和其他路由，返回index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
